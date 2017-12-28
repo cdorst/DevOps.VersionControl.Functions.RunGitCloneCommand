d8e5bbf41ad0f32c580b02708ddfe8399b4e9485
Christopher Dorst <chris@dorst.email>
2017-12-28 12:12:17 -0600

    Add GitCloneCommandRunner

---
+++ DevOps.VersionControl.Functions.RunGitCloneCommand/GitCloneCommandRunner.cs
@@ -0,0 +1,14 @@
+﻿using static DevOps.VersionControl.Functions.RunGitCommand.GitCommandRunner;
+
+namespace DevOps.VersionControl.Functions.RunGitCloneCommand
+{
+    public static class GitCloneCommandRunner
+    {
+        private const string clone = nameof(clone);
+
+        public static void Clone(string directory, string uri)
+            => Git(directory,
+                command: clone,
+                uri);
+    }
+}
